7a64da92a67117d786e78a6ae1e2fc4b94ef097f
Daniel Steuernol <daniel@sendwithus.com>
2018-02-26 13:13:55 -0800

    make a call to /end when the game ends

---
+++ lib/bs_web/views/end_view.ex
@@ -0,0 +1,32 @@
+defmodule BsWeb.EndView do
+  alias BsWeb.Api.DeathView
+
+  use BsWeb, :view
+
+  def render("show.json", %{
+        winners: winners,
+        deadSnakes: deadSnakes,
+        gameId: gameId
+      }) do
+    %{
+      gameId: gameId,
+      winners: winners,
+      deadSnakes: %{
+        object: :list,
+        data:
+          Enum.map(deadSnakes, fn snake ->
+            %{
+              id: snake.id,
+              length: Enum.count(snake.coords),
+              death:
+                render_one(
+                  snake.death,
+                  DeathView,
+                  "show.json"
+                )
+            }
+          end)
+      }
+    }
+  end
+end
